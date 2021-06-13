bef165302973bc712f1a7c5162b7e57f8bbc0604
Shubham Agrawal <shubhs0707@gmail.com>
2021-06-13 17:43:42 +0530

    day 4 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coco_catalog/screens/home_page.dart';
 import 'package:coco_catalog/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
       debugShowCheckedModeBanner: false,
       title: 'Coco Catalog',
+      initialRoute: '/login',
       routes: {
         '/': (context) => HomePage(),
         '/login': (context) => LoginPage()
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -6,8 +6,52 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text('LoginPage'),
+      child: Column(
+        children: [
+          Image.asset('assets/images/1.jpg'),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Shubham',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 10,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter username',
+                    labelText: 'username',
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    labelText: 'password',
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('login pressed');
+                  },
+                  child: Text('Login'),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
