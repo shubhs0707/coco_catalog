ed85cb1aad274d5591b744fd706ddadd12433329
Shubham Agrawal <shubhs0707@gmail.com>
2021-06-19 13:46:10 +0530

    day 11 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -8,3 +8,14 @@ class Item {
 
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
+
+final products = [
+  Item(
+    color: "#33505a",
+    desc: "Apple 12 " * 5,
+    id: "Shubham101",
+    name: "IPhone 12 Pro",
+    image: "",
+    price: 12999,
+  ),
+];
